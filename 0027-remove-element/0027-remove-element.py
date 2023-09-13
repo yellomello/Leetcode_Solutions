90f12fd026a49dad4f54c595bc4b198c5e24e96a
Marc Hansel John Thomas <35078301+yellomello@users.noreply.github.com>
2023-09-13 09:53:22 -0400

    Time: 15 ms (75.31%), Space: 13 MB (99.54%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        count = 0
+        # Loop through all the elements of the array
+        for i in range(len(nums)):
+            # If the element is not val
+            if nums[i] != val:
+                nums[count] = nums[i]
+                count += 1
+        return count
