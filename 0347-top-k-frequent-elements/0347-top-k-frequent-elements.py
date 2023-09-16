382708342192bf1a5cfb2ace3b8188502cebff0f
Marc Hansel John Thomas <35078301+yellomello@users.noreply.github.com>
2023-09-16 14:57:17 -0400

    Added my initial solution that didn't work.

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -14,3 +14,24 @@ class Solution(object):
                 res.append(n)
                 if len(res) == k:
                     return (res)
+
+
+
+#My First solution that didn't work:
+'''
+
+d={}
+for i in range(len(nums)):
+  # if nums[i] not in d:
+  #   d[nums[i]]=1
+  # else: 
+  #   d[nums[i]]+=1
+  if nums[i] in d:
+    d[nums[i]]+=1
+  else: 
+    d[nums[i]]=1
+sorted_dict = dict(sorted(d.items(), key=lambda item: item[1],reverse=True))
+first_two_keys = list(sorted_dict.keys())[:k]
+return(first_two_keys)
+
+'''
